e168a01a1e1f8f28b2a0b00a7b06ad02f707365c
Tolga İskender <tolga@basefy.com>
2020-02-09 14:44:36 +0300

    ImageDetection with CoreML by using Resnet50

--- ObjectDetectionWithCoreML/ViewController.swift
+++ ObjectDetectionWithCoreML/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
-
+import AVKit
+import Vision
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outPutLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        settingUpCamera()
     }
-
-
+    
+    func settingUpCamera(){
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        guard let input  = try? AVCaptureDeviceInput(device: device) else { return }
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        let previewLayer =  AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+    }
+    
+    
+}
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer:CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            
+            guard let result = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let firstObs = result.first else { return }
+            DispatchQueue.main.async {
+                self.outPutLabel.text = "\(firstObs.identifier) \(firstObs.confidence * 100)%"
+            }
+            
+            
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        
+    }
+    
 }
 
